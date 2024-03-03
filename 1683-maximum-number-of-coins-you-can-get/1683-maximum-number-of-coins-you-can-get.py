9749e5ba61976c217a77ee8798f76336189e94ed
Ab_el <133862280+abeelgetahun@users.noreply.github.com>
2024-03-03 19:41:58 +0300

    Time: 479 ms (50.27%), Space: 28.9 MB (57.47%) - LeetHub

---
+++ 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        # this is ("ab_el")
+        piles.sort()
+        count=0
+        i=0 # count form left of the input arr
+        j=len(piles)-1 # count form the right of the input arr
+        while i<j:
+            count+=piles[j-1]
+            j-=2
+            i+=1
+        return count
+
+
+
