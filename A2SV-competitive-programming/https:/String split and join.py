687ea8150f1759d24f132729b66424a5253421d8
Ab_el <133862280+abeelgetahun@users.noreply.github.com>
2024-03-24 01:52:22 +0300

    Delete A2SV-competitive-programming/https: directory

--- A2SV-competitive-programming/https:/String split and join.py
+++
@@ -1,9 +0,0 @@
-https://www.hackerrank.com/challenges/python-string-split-and-join?isFullScreen=true
-def split_and_join(line):
-    # write your code here
-    line=line.split()
-    return "-".join(line)
-if __name__ == '__main__':
-    line = input()
-    result = split_and_join(line)
-    print(result)
