15573aaebf7074b4985f0824582e9ad6bc386404
Ab_el <133862280+abeelgetahun@users.noreply.github.com>
2024-03-30 10:27:17 +0300

    Update 1309-decrypt-string-from-alphabet-to-integer-mapping.py

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,8 +1,10 @@
+# this is ("ab_el")
 class Solution:
     def freqAlphabets(self, s: str) -> str:
         alpha=""
         s=list(s)
         i,y=0,len(s)
+        # this part help to change each number to character list value separate using #
         while i<y:
             if s[i]=="#":
                 temp=s[i-2]+s[i-1]
@@ -12,6 +14,7 @@ class Solution:
                 i-=2
                 y-=2
             i+=1
+        # this part used to covert list members to character (eg.a=97)
         for i in s:
             alpha+=chr(int(i)+96)
         return alpha
