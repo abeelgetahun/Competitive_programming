f8657ac44e57d8c6a6ac907efdadd7b32738370b
Ab_el <133862280+abeelgetahun@users.noreply.github.com>
2024-04-09 17:27:29 +0300

    Create 283. Move Zeroes.py

---
+++ leetcode/283. Move Zeroes/283. Move Zeroes.py
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/move-zeroes/
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        place_holder=0
+        seeker=0
+        while seeker<len(nums):
+            if nums[seeker]!=0 :
+                nums[place_holder],nums[seeker]=nums[seeker],nums[place_holder]
+                place_holder+=1
+            seeker+=1
+        holder = 0
+       
