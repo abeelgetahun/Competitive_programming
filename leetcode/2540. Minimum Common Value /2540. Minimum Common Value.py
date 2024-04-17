e5d1f73393a1aea8a626e4c83ef50d15e1bff895
Ab_el <133862280+abeelgetahun@users.noreply.github.com>
2024-04-17 11:35:13 +0300

    Delete leetcode/2540. Minimum Common Value  directory

--- leetcode/2540. Minimum Common Value /2540. Minimum Common Value.py
+++
@@ -1,8 +0,0 @@
-#https://leetcode.com/problems/minimum-common-value/
-class Solution:
-    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        u=set(nums1).intersection(set(nums2))
-        u=list(u)
-        return min(u) if len(u)>0 else -1
-
-        
